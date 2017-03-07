2d086d3e6ae841078d04edbc99c62f68eb991a30
Nong <naong69@gmail.com>
2017-03-07 12:26:09 +0700

    v1.9.3
    fix color maching in get_validate.php

--- admin/get_validate.php
+++ admin/get_validate.php
@@ -21,17 +21,33 @@ if(isset($_REQUEST['time'])){
 		$sql = "SELECT SUBSTRING(`build_up_index`, 1, 1) as type, COUNT(*)AS count FROM `validate_logs` WHERE `time_stamp` BETWEEN DATE_ADD(CURDATE(), INTERVAL -".$time." DAY) AND CURDATE() GROUP BY SUBSTRING(`build_up_index`, 1, 1) ORDER by type";
 	}
 	if ($result=mysqli_query($link,$sql)){
-		$i=0;
 		while ($row=mysqli_fetch_row($result)){
 			$cat_count = $cat_count.$row[1].",";
-			$cat_color = $cat_color.'"'.$colorArray[$i].'",';
-			if($row[0] == 'B') {$cat_label = $cat_label.'"อาคารพาณิช",';}
-			if($row[0] == 'F') {$cat_label = $cat_label.'"โรงงาน",';}
-			if($row[0] == 'G') {$cat_label = $cat_label.'"สถานที่เกี่ยวกับน้ำมันและก๊าซ",';}
-			if($row[0] == 'I') {$cat_label = $cat_label.'"โครงสร้างพื้นฐาน",';}
-			if($row[0] == 'L') {$cat_label = $cat_label.'"อาคารที่อยู่อาศัย",';}
-			if($row[0] == 'P') {$cat_label = $cat_label.'"อาคารสาธารณะ",';}
-			$i++;
+			
+			if($row[0] == 'B') {
+				$cat_label = $cat_label.'"อาคารพาณิช",';
+				$cat_color = $cat_color.'"#FF6384",';
+			}
+			if($row[0] == 'F') {
+				$cat_label = $cat_label.'"โรงงาน",';
+				$cat_color = $cat_color.'"#983ef2",';
+			}
+			if($row[0] == 'G') {
+				$cat_label = $cat_label.'"สถานที่เกี่ยวกับน้ำมันและก๊าซ",';
+				$cat_color = $cat_color.'"#f44242",';
+			}
+			if($row[0] == 'I') {
+				$cat_label = $cat_label.'"โครงสร้างพื้นฐาน",';
+				$cat_color = $cat_color.'"#36A2EB",';
+				}
+			if($row[0] == 'L') {
+				$cat_label = $cat_label.'"อาคารที่อยู่อาศัย",';
+				$cat_color = $cat_color.'"#3ef27a",';
+			}
+			if($row[0] == 'P') {
+				$cat_label = $cat_label.'"อาคารสาธารณะ",';
+				$cat_color = $cat_color.'"#FFCE56",';
+			}
 		}
 	} 
 	// Free result set
